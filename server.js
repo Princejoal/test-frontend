fefe931481626dbb5be62c1be58c127e07bf8f66
Lella Chandu sai <35798511+chandulella23@users.noreply.github.com>
2018-12-22 16:57:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express=require('express');
 const path=require('path');
 const app=express();
 
-app.use(express.static(__dirname+'/dist/frntend'));
 
-app.get(path.join(__dirname + '/dist/frntend/index.html'));
+// app.use(express.static(__dirname+'/dist/frntend'));
+app.use(express.static(__dirname, 'dist', {index: false}));
+
+// app.get(path.join(__dirname + '/dist/frntend/index.html'));
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8080,()=>{
     console.log("Successful")
