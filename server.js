4f773ec3e44bc30240511de8bcd06ec3cb990adf
Lella Chandu sai <35798511+chandulella23@users.noreply.github.com>
2018-12-22 17:17:05 +0530

    done i think finnaly dynamic routes

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const app=express();
 app.use(express.static(__dirname+'/dist/frntend', {index: false}));
 
 // app.get(path.join(__dirname + '/dist/frntend/index.html'));;
-app.get('', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/frntend/index.html'));
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'dist/frntend', 'index.html'));
 });
+// app.get('', function(req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/frntend/index.html'));
+// });
 app.listen(process.env.PORT || 8080,()=>{
     console.log("Successful")
 })
